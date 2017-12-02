d29f2e1fb104417cf29b2177774a72aa8f5eee6a
amcnulty <amcnulty88@swbell.net>
2017-12-01 16:51:59 -0800

    DB Connection
    Working on connecting to DB in heroku.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+if (process.env.NODE_ENV === 'development') {
+    var connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
+else {
+    var connection = mysql.createConnection({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
 
 connection.connect();
 
