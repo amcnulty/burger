300a19cdcdebb8e7141234f3c1c395602eedea28
amcnulty <amcnulty88@swbell.net>
2017-12-02 14:58:04 -0800

    Bug Fixes
    Working on fixing a bug that is causing the app to crash from database disconnects.

--- config/connection.js
+++ config/connection.js
@@ -20,22 +20,4 @@ else {
 
 connection.connect();
 
-connection.query('SET @@auto_increment_increment=1;');
-
-connection.query('INSERT INTO burgers(burger_name)VALUES("Crabby Patty");');
-
-connection.query('INSERT INTO burgers(burger_name)VALUES("Krusty Burger");');
-
-connection.query('INSERT INTO burgers(burger_name)VALUES("Honker Burger");');
-
-
-connection.on('error', function (err) {
-    console.log('db error', err);
-    // if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-    //     handleDisconnect();                         // lost due to either server restart, or a
-    // } else {                                      // connnection idle timeout (the wait_timeout
-    //     throw err;                                  // server variable configures this)
-    // }
-});
-
 module.exports = connection;
